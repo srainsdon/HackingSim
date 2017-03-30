f703514050550c51331b499e8a8c59829c9b1274
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 14:30:28 -0700

    admin
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/adminCommands.class.php
+++ classes/adminCommands.class.php
@@ -14,7 +14,7 @@ class adminCommands
     {
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
         $opt = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
