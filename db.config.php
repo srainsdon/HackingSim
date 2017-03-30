38a253f4bc1b73a69304338f52dadaf919d9b691
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 13:52:40 -0700

    gettit it
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

---
+++ db.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: srainsdon
+ * Date: 3/29/2017
+ * Time: 10:56 PM
+ */
+
+$host = 'localhost';
+$db = 'id1216072_gamesim';
+$user = 'id1216072_srainsdon';
+$pass = 'N0cand0a';
