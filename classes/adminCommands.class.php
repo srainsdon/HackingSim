df85f0d3e1c3767b8ac90ff06f0a1f5f253b8f33
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 14:26:25 -0700

    admin
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/adminCommands.class.php
+++ classes/adminCommands.class.php
@@ -22,8 +22,9 @@ class adminCommands
     }
 
     function createComputer($hostName, $domain, $ipaddress) {
-        $sql = "INSERT INTO Computers (`ComputerHostName`,  `ComputerDomain`,  `ComputerIP`,)"
+        $sql = "INSERT INTO Computers ('ComputerHostName',  'ComputerDomain',  'ComputerIP',)"
                   ." VALUES ('$hostName', '$domain', '".ip2long($ipaddress)."')";
+        echo $sql;
         if ($this->pdo->query($sql) === TRUE) {
             $last_id = $this->pdo->insert_id;
             echo "New record created successfully. Last inserted ID is: " . $last_id;
