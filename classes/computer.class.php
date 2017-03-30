8df5b9e08e43405a509c24a71a4a65900213ffac
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 05:28:10 -0700

    finished off line

--- classes/computer.class.php
+++ classes/computer.class.php
@@ -34,26 +34,46 @@ class computer
     private $hostName;
     private $domainName;
     private $ip;
+    private $id;
+    private $pwd;
+    private $fs;
 
-    /*function __construct(sqlManager $sql, $compID)
-    {
-        $info = $sql->getComputerByID($compID);
-        $this->ip = long2ip($info['ComputerIP']);
-        $this->hostName = $info['ComputerHostName'];
-        $this->domainName = $info['ComputerDomain'];
-        $this->settings = json_decode($info['ComputerSetup'], true);
-        $this->fileSystem = new fileSystem(json_decode($info['ComputerFiles'], true));
-    }*/
+    // Offline Data
+    private $computers = array(
+        1 => array(
+            'ip' => "8.8.8.8",
+            'hostName' => 'dns-1',
+            'domainName' => 'google.com',
+            'settings' => '{"services":{"ssh":{"version":"1.0","users":{"root":"1234567890","baduser":"1234567890"},"logging":{"file":"\/var\/log\/ssh.log","level":"debug"}},"http":{"version":"2.0","logging":{"file":"\/var\/log\/apache2.log","level":"error"}}},"firewall":{"ping":1,"22":1,"80":1}}',
+            'fileSystem' => '{"var":{"_type":"D","log":{"_type":"D","ssh.log":{"_type":"F","data":"[2017 03 29 23:35] root logged in from 120.52.17.2"}},"www":{"_type":"D","index.html":{"_type":"F","data":"<!-- <html><head><title>Google Dns Server<\/title><\/head><body><h1>Go away.<\/h1><\/body><\/html> -->"}}},"etc":{"_type":"D","bin":{"_type":"D","bind":{"_type":"F","data":"Binary Program Data..."},"apache2":{"_type":"F","data":"Binary Program Data..."}}}}'
+        )
 
-    function __construct(sqlManager $sql, $compID)
+    );
+
+    function save()
     {
-        $info = $sql->getComputerByID($compID);
-        $this->ip = long2ip($info['ComputerIP']);
-        $this->hostName = $info['ComputerHostName'];
-        $this->domainName = $info['ComputerDomain'];
-        $this->settings = json_decode($info['ComputerSetup'], true);
-        $this->fileSystem = new fileSystem(json_decode($info['ComputerFiles'], true));
+        return json_encode(array($this->id, $this->fileSystem->cmd_pwd()));
     }
+
+    function __construct(sqlManager $sql, $compID, $pwd = '/')
+    {
+        $info = $sql->getComputerByID($compID);
+        $this->id = $compID;
+        $this->ip = long2ip($info['ComputerIP']);
+        $this->hostName = $info['ComputerHostName'];
+        $this->domainName = $info['ComputerDomain'];
+        $this->settings = json_decode($info['ComputerSetup'], true);
+        $this->fileSystem = new fileSystem(json_decode($info['ComputerFiles'], true), $pwd);
+    }
+
+    /*function __construct($sql, $compID)
+    {
+            $this->ip = $this->computers[$compID]['ip'];
+            $this->hostName = $this->computers[$compID]['hostName'];
+            $this->domainName = $this->computers[$compID]['domainName'];
+            $this->settings = json_decode($this->computers[$compID]['settings'], true);
+            $this->fileSystem = new fileSystem(json_decode($this->computers[$compID]['fileSystem'], true));
+    }*/
 
     function getComputerInfo()
     {
--- index.php
+++ index.php
@@ -6,35 +6,8 @@
  * Time: 4:36 AM
  */
 include_once 'config.php';
-header("Content-type:text/plain");
-// $sql = new sqlManager();
-// $computer = new computer("", "1");
-$settings = array(
-    "services" => array(
-        "ssh" => array(
-            'version' => '1.0',
-            "users" => array(
-                'root' => '1234567890',
-                'baduser' => '1234567890'),
-            'logging' => array(
-                "file" => '/var/log/ssh.log',
-                "level" => 'debug'
-            )
-        ),
-        "http" => array(
-            "version" => '2.0',
-            'logging' => array(
-                "file" => '/var/log/apache2.log',
-                "level" => 'error'
-            )
-        )
-    ),
-    "firewall" => array(
-        "ping" => 1,
-        22 => 1,
-        80 => 1
-    )
-);
-print_r($settings);
-echo "\n\n\n\n" . json_encode($settings);
-// $computer->fileSystem->showFileSystem();
+header("Content-type:text/html");
+
+$computer = new computer($sql, "1");
+$data = $computer->save();
+echo $data;
