d3c933548804142bdee8e8d0e5cd9b3c3501b175
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-29 23:28:45 -0700

    Starting to pull it apart for off line No DB
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- index.php
+++ index.php
@@ -7,6 +7,34 @@
  */
 include_once 'config.php';
 header("Content-type:text/plain");
-$sql = new sqlManager();
-$computer = new computer($sql, "3");
-$computer->fileSystem->showFileSystem();
+// $sql = new sqlManager();
+// $computer = new computer("", "1");
+$settings = array(
+    "services" => array(
+        "ssh" => array(
+            'version' => '1.0',
+            "users" => array(
+                'root' => '1234567890',
+                'baduser' => '1234567890'),
+            'logging' => array(
+                "file" => '/var/log/ssh.log',
+                "level" => 'debug'
+            )
+        ),
+        "http" => array(
+            "version" => '2.0',
+            'logging' => array(
+                "file" => '/var/log/apache2.log',
+                "level" => 'error'
+            )
+        )
+    ),
+    "firewall" => array(
+        "ping" => 1,
+        22 => 1,
+        80 => 1
+    )
+);
+print_r($settings);
+echo "\n\n\n\n" . json_encode($settings);
+// $computer->fileSystem->showFileSystem();
