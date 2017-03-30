ccf58f938caa7ac3fb9867483f20d5b99eba8e03
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 05:53:53 -0700

    working out the admin section

---
+++ admin/admin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: srainsdon
+ * Date: 3/30/2017
+ * Time: 5:52 AM
+ */
+
+include_once '../config.php';
+
+$form = new PFBC\Form("GettingStarted");
+$form->addElement(new PFBC\Element\Textbox("My Textbox:", "MyTextbox"));
+$form->addElement(new PFBC\Element\Select("My Select:", "MySelect", array(
+    "Option #1",
+    "Option #2",
+    "Option #3"
+)));
+$form->addElement(new PFBC\Element\Button);
+$form->render();
