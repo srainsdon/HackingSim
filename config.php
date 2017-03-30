1de48a217ffaffda33b60b2b2b675893174b88b1
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 05:51:31 -0700

    fixed db creds

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ require 'vendor/autoload.php';
 
 include_once './classes/sqlManager.class.php';
 include_once '../config/db.config.php';
-$sql = new sqlManager($host, $db, $user, $pass);
+$sql = new sqlManager('34.208.253.55', 'HackingSim', 'srainsdon', 'N0cand0a');
 include_once './classes/computer.class.php';
 include_once './classes/random.class.php';
 include_once './classes/fileSystem.class.php';
