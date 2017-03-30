2901b02f993b1b6f7dcb2d60de47fac35b7db85a
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 08:20:22 -0700

    extra comma
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- admin/admin.php
+++ admin/admin.php
@@ -10,10 +10,7 @@ include_once '../config.php';
 session_start();
 $computer = new computer($sql, "1");
 $data = $computer->getData();
-/* Get Nibble Forms 2 instance called mega_form */
-$form = \Nibble\NibbleForms\NibbleForm::getInstance('ComputerID1','',true,'post','Submit',"table");
 
-/* Text field with custom class and max length attribute */
 $smarty->assign('data', $data);
 
 $smarty->display('computer.tpl');
