5f4077b41096ed979820651b9933b9124a5d4133
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 05:59:57 -0700

    working out admin things
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- config.php
+++ config.php
@@ -10,12 +10,12 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require 'vendor/autoload.php';
-
-include_once './classes/sqlManager.class.php';
-include_once '../config/db.config.php';
+$base = $_SERVER["DOCUMENT_ROOT"];
+include_once '$base/classes/sqlManager.class.php';
+include_once '$base/../config/db.config.php';
 $sql = new sqlManager('34.208.253.55', 'HackingSim', 'srainsdon', 'N0cand0a');
-include_once './classes/computer.class.php';
-include_once './classes/random.class.php';
-include_once './classes/fileSystem.class.php';
+include_once '$base/classes/computer.class.php';
+include_once '$base/classes/random.class.php';
+include_once '$base/classes/fileSystem.class.php';
 
 // include_once("dBug.php");
