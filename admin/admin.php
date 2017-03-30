e08c69a11a1cf25a1938f0ba89a4558864771820
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 07:13:28 -0700

    getting there
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- admin/admin.php
+++ admin/admin.php
@@ -8,11 +8,12 @@
 
 include_once '../config.php';
 session_start();
+$computer = new computer($sql, "1");
 /* Get Nibble Forms 2 instance called mega_form */
-$form = \Nibble\NibbleForms\NibbleForm::getInstance('mega_form');
+$form = \Nibble\NibbleForms\NibbleForm::getInstance('ComputerID1');
 
 /* Text field with custom class and max length attribute */
-$form->addField('text_field', 'text', array(
+$form->addField('IP_Address', 'text', array(
     'class' => 'testy classes',
     'max_length' => 20
 ));
@@ -37,15 +38,19 @@ $form->addField('choice', 'radio', array(
 
 
 echo "<!DOCTYPE html>\n<head>\n<title>Nibble Forms Demo</title>\n"
-    ."<script src=\"http://www.google.com/jsapi\" type=\"textjavascript\"></script>\n"
-    ."<link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"style.css\" />\n"
-    ."<!-- Bootstrap core CSS -->\n"
-    ."<link href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\">\n"
-    ."</head><body>\n";
+    . "<script src=\"http://www.google.com/jsapi\" type=\"textjavascript\"></script>\n"
+    . "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"style.css\" />\n"
+    . "<!-- Bootstrap core CSS -->\n"
+    . "<link href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\">\n"
+    . "</head><body>\n";
 
 
 // If the form is valid, do something
-
+$data = $computer->getComputerInfo();
+$form->addData(array(
+        "IP_Address" => $data['IP Address'],
+    )
+);
 echo $form->render();
 echo "\n<script src=\"//code.jquery.com/jquery-3.1.1.min.js\" crossorigin=\"anonymous\"></script>\n</body>\n</html>";
 
