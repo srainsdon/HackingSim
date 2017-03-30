b4668775c5a2365bc4fc8765581a4186766027ae
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-30 06:27:19 -0700

    Nibble-Forms
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- admin/admin.php
+++ admin/admin.php
@@ -8,13 +8,34 @@
 
 include_once '../config.php';
 session_start();
-include("$base/vendor/pfbc/pfbc/PFBC/Form.php");
-$form = new PFBC\Form("GettingStarted");
-$form->addElement(new PFBC\Element\Textbox("My Textbox:", "MyTextbox"));
-$form->addElement(new PFBC\Element\Select("My Select:", "MySelect", array(
-    "Option #1",
-    "Option #2",
-    "Option #3"
-)));
-$form->addElement(new PFBC\Element\Button);
-$form->render();
+/* Get Nibble Forms 2 instance called mega_form */
+$form = \Nibble\NibbleForms\NibbleForm::getInstance('mega_form');
+
+/* Text field with custom class and max length attribute */
+$form->addField('text_field', 'text', array(
+    'class' => 'testy classes',
+    'max_length' => 20
+));
+
+/* Email field, not required and custom label text */
+$email = $form->addfield('email', 'email', array(
+    'required' => false,
+    'label' => 'Please enter your email address'
+));
+/* Email confirmation field which must match the value for email */
+$email->addConfirmation('confirm_email', array(
+    'label' => 'Please confirm your email address'
+));
+
+/* Radio button field with two options, first option has an additional attribute */
+$form->addField('choice', 'radio', array(
+    'choices' => array(
+        "one" => array('data-example' => 'data-attribute-value', 'Choice One'),
+        "two" => "Choice Two"),
+    'false_values' => array("two")
+));
+echo $form->render();
+/* If the form is valid, do something */
+if ($form->validate()) {
+    echo "Form has validated";
+}
