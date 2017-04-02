9581fbd48fe5c1c1649006c27ae3df2eca0948cb
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-02 05:24:30 -0700

    Index.php
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: srainsdon
+ * Date: 4/2/2017
+ * Time: 5:19 AM
+ */
+
+include_once 'admin.config.php';
+
+$smarty->assign('links', array(
+    'Computers' => './computer.php',
+    'Networks' => './network.php',
+));
+
+$smarty->display('home.tpl');
---
+++ admin/index.tpl
@@ -0,0 +1,6 @@
+{extends file="main.tpl"}
+{block name=body}
+    {foreach $links as $name => $link}
+        <h3><a href="{$link}">{$name}</a></h3>
+    {/foreach}
+{/block}
