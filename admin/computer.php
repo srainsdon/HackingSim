bc8bf93db6881274bda8831c104e5875328e0648
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-02 06:54:10 -0700

    Index.php
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- admin/computer.php
+++ admin/computer.php
@@ -6,16 +6,20 @@
  * Time: 5:52 AM
  */
 include_once 'admin.config.php';
-
-$data = $sql->getFixedIPs();
-$newData = array();
-foreach ($data as $row) {
-    $row['NewLong'] = rand($row['NetworkStart'], $row['NetworkEnd']);
-    $row['NewIP'] = long2ip($row['NewLong']);
-    $newData[] = $row;
+if (isset($_GET['fix'])) {
+    $data = $sql->getFixedIPs();
+    $newData = array();
+    foreach ($data as $row) {
+        $row['NewLong'] = rand($row['NetworkStart'], $row['NetworkEnd']);
+        $row['NewIP'] = long2ip($row['NewLong']);
+        $newData[] = $row;
+    }
+    $smarty->assign("bCrumbs", "Fix Ip List");
+    $smarty->assign("computers", $newData);
+    $smarty->display('Fixlist.tpl');
+} else {
+    $smarty->assign("bCrumbs", "Full Computer List");
+    $smarty->assign("computers", $sql->getAllComputers());
+    $smarty->assign("computers", $newData);
+    $smarty->display('list.tpl');
 }
-
-$smarty->assign("bCrumbs", "Full Computer List");
-//$smarty->assign("computers", $sql->getAllComputers());
-$smarty->assign("computers", $newData);
-$smarty->display('Fixlist.tpl');
