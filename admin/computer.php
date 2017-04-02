99739816aae118db3cabc3b958539b2ce889fa0b
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-02 06:20:14 -0700

    Index.php
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- admin/computer.php
+++ admin/computer.php
@@ -5,14 +5,14 @@
  * Date: 3/30/2017
  * Time: 5:52 AM
  */
-
+include_once 'admin.config.php';
 function newIP($prams, Smarty_Internal_Template $template)
 {
     return long2ip(rand($prams['start'], $prams['end']));
 }
 
 $smarty->registerPlugin("function", "newIP", "newIP");
-include_once 'admin.config.php';
+
 $smarty->assign("bCrumbs", "Full Computer List");
 //$smarty->assign("computers", $sql->getAllComputers());
 $smarty->assign("computers", $sql->getFixedIPs());
