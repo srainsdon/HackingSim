27d6f9eb9e3ec13e73a3df897e553e46ed63426f
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-03 05:05:25 -0700

    reworked composer autoload
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- index.php
+++ index.php
@@ -5,24 +5,7 @@
  * Date: 3/29/2017
  * Time: 4:36 AM
  */
+include_once 'config.php';
+header("Content-type:text/html");
 
-// header("Content-type:text/html");
-
-
-function walk_dir($dir)
-{
-    $relativedir = '.' . $dir;
-    if ($dh = opendir($relativedir)) {
-        while (false !== ($file = readdir($dh))) {
-            if (($file !== '.') && ($file !== '..')) {
-                if (!is_dir($relativedir . $file)) {
-                    echo '<a href="' . $dir . $file . '" title="' . $file . '">' . $file . '</a>' . "\n";
-                } else {
-                    walk_dir($dir . $file . '/');
-                }
-            }
-        }
-    }
-}
-
-walk_dir('/');
+$sql->getAllComputers();
