036e8d0e67a867f76e02246d39b5857d66121ffa
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-03 03:37:10 -0700

    Single Computer listing fixing sql statement
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/sqlManager.class.php
+++ classes/sqlManager.class.php
@@ -51,7 +51,7 @@ class sqlManager
 
     function getComputerByID($id)
     {
-        $stmt = $this->pdo->query("SELECT * FROM Computers where ComputerID = $id");
+        $stmt = $this->pdo->query("SELECT Computers.ComputerID, CONCAT(Computers.ComputerHostName, '.', Computers.ComputerDomain) AS ComputerName, Computers.ComputerHostName, Computers.ComputerDomain, INET_NTOA(Computers.ComputerIP) AS ComputerIP, Networks.NetworkName FROM Computers, Networks WHERE Computers.ComputerNetwork = Networks.NetworkID and Computers.ComputerID = $id");
         $computer = $stmt->fetch();
         return $computer;
     }
