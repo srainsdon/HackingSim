3856cb4f9b5c3dcd40655216dfdc8cd9645739ad
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-03 15:14:28 -0700

    Switching over to mod_rewrite and using a pubic/ folder as Doc root
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@
 include_once '../config.php';
 
 $smarty->assign('links', array(
-    'Computers' => './computer.php',
-    'Networks' => './network.php',
+    'Computers' => './computer/',
+    'Networks' => './network/',
 ));
 
 $smarty->display('index.tpl');
