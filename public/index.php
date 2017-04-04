413cb7d4c24dcdb6fcb3c9c1f3b83671509bdfc4
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-04 03:01:43 -0700

    yep
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/index.php
+++ public/index.php
@@ -13,26 +13,33 @@ if (isset($_GET['data'])) {
     $smarty->assign('data', $_GET['data']);
     $cmd = explode('/', $_GET['data']);
     $smarty->assign('Cmd:', $cmd);
-    switch ($cmd[0]) {
-        case "admin": {
-            $smarty->append('bCrumbs', "<a href='/admin'>Admin</a>");
-            switch ($cmd[1]) {
-                case
-                "computer":
-                    $computerId = $cmd[2];
-                    include_once 'admin/computer.php';
-                    break;
-                case "network":
-                    include_once 'admin/network.php';
-                    break;
-                default:
-                    include_once 'admin/home.php';
-                    break;
-            }
+    if (isset($cmd[0]) && $cmd[0] == "admin") {
+        $smarty->append('bCrumbs', "<a href='/admin'>Admin</a>");
+        switch ($cmd[1]) {
+            case "computer":
+                $computerId = $cmd[2];
+                include_once 'admin/computer.php';
+                break;
+            case "network":
+                include_once 'admin/network.php';
+                break;
+            default:
+                include_once 'admin/home.php';
+                break;
         }
-        default:
-            include_once 'home.php';
-            break;
+    } else {
+        switch ($cmd[0]) {
+            case "computer":
+                $computerId = $cmd[2];
+                include_once 'admin/computer.php';
+                break;
+            case "network":
+                include_once 'admin/network.php';
+                break;
+            default:
+                include_once 'home.php';
+                break;
+        }
     }
 } else {
     include_once 'home.php';
