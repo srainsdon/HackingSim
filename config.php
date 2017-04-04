0aaf1a7a91c5d880a11bbb2a6543030412b6cb10
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-04 16:14:45 -0700

    fixing bootstrap
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- config.php
+++ config.php
@@ -18,4 +18,4 @@ $smarty = new Smarty_HackingSim;
 
 $config = new PHPAuth\Config($sql->getPdo());
 $auth = new PHPAuth\Auth($sql->getPdo(), $config);
-$smarty->assign('LogedIn', Flase);
+$smarty->assign('LogedIn', False);
