8819e47a8d5b4a9f80569204ae2751c84c567bc3
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-04 17:58:21 -0700

    fixing bootstrap
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/login.php
+++ public/login.php
@@ -6,26 +6,24 @@
  * Time: 4:27 PM
  */
 
-if (!isset($_SESSION['authID']) && isset($_POST['email'])) {
+if (!isset($_COOKIE['authID']) && isset($_POST['email'])) {
     $message = "";
     $message .= "Post:\n" . print_r($_POST, true);
     $loginInfo = $auth->login($_POST['email'], $_POST['pass']);
     $message .= "Login Info:\n" . print_r($loginInfo, true);
-    $_SESSION['authID'] = $loginInfo['hash'];
+    $_COOKIE['authID'] = $loginInfo['hash'];
     $smarty->assign('alert', $loginInfo['message']);
-    $message .= "Session:\n" . print_r($_SESSION, True);
+    $message .= "Session:\n" . print_r($_COOKIE, True);
     $smarty->assign('message', $message);
-} elseif (isset($_SESSION['authID'])) {
-    $message = "Is session['authID'] Good. " . $auth->checkSession($_SESSION['authID']) . " Is Logged In? " . $auth->isLogged() . "\n";
+} elseif (isset($_COOKIE['authID'])) {
+    $message = "Is session['authID'] Good. " . $auth->checkSession($_COOKIE['authID']) . " Is Logged In? " . $auth->isLogged() . "\n";
     $message .= "Post:\n" . print_r($_POST, true);
     $message .= "Login Info:\n" . print_r($loginInfo, true);
-    $_SESSION['authID'] = $loginInfo['hash'];
-    $smarty->assign('alert', $loginInfo['message']);
-    $message .= "Session:\n" . print_r($_SESSION, True);
+    $message .= "Session:\n" . print_r($_COOKIE, True);
     $smarty->assign('message', $message);
 }
-if (isset($_SESSION)) {
-    $smarty->assign('body', print_r($_SESSION, true));
+if (isset($_COOKIE)) {
+    $smarty->assign('body', print_r($_COOKIE, true));
 }
 $smarty->append('bCrumbs', "<span class=\"breadcrumb-item active\">Login</span>");
 $smarty->display('login.tpl');
