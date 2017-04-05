e0756de1ae321fd810654c82fcbbd56a3751fb48
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-04 17:06:17 -0700

    fixing bootstrap
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/login.php
+++ public/login.php
@@ -7,9 +7,9 @@
  */
 
 if (!isset($_SESSION['hash']) && isset($_POST['email'])) {
-    $smarty->assign($_POST);
+    $smarty->assign('message', $_POST);
     $_SESSION['hash'] = $auth->login($_POST['email'], $_POST['pass']);
-} else {
+} elseif (isset($_SESSION['hash'])) {
     $smarty->assign('body', $auth->checkSession($_SESSION['hash']));
 }
 
