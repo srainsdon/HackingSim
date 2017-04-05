0404e0efc2fb89741e915b97f7866f8477acb433
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-05 10:13:15 -0700

    login system
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/userManager.class.php
+++ classes/userManager.class.php
@@ -8,17 +8,13 @@
  */
 class userManager extends PHPAuth\Auth
 {
-
+    /*
     public function __construct(\PDO $dbh, $config)
     {
         parent::__construct($dbh, $config);
-        $sql = "SELECT permisionID, permisionName FROM permisions";
-        foreach ($this->dbh->query($sql)->fetchAll() as $key => $value) {
-            define($value, $key);
-        }
     }
-
-    public function isAutherised($level)
+    */
+    public function isAuthorised($level)
     {
         $sql = "SELECT permisions.permisionName FROM users JOIN userGroup ON userGroup.userID = users.id JOIN groupPermisions ON groupPermisions.gpGroup = userGroup.groupID JOIN permisions ON groupPermisions.gpPermision = permisions.permisionID WHERE users.id = 1";
         $permisions = $this->dbh->query($sql)->fetchAll();
