c485c93506c9e3f2323ea77ae8587566f9eb6f07
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-06 20:42:58 -0700

    removed $this
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/admin/logs.php
+++ public/admin/logs.php
@@ -5,11 +5,11 @@
  * Date: 4/6/2017
  * Time: 7:41 PM
  */
-$this->smarty->append('bCrumbs', "<span class=\"breadcrumb-item active\">Log Reader</span>");
+$smarty->append('bCrumbs', "<span class=\"breadcrumb-item active\">Log Reader</span>");
 if ($extras->isRemoteDev()) {
-    $this->smarty->append('body', 'You are a dev!');
+    $smarty->append('body', 'You are a dev!');
 }
 foreach ($extras->getLogList() as $logFile) {
-    $this->smarty->append('body', $logFile);
+    $smarty->append('body', $logFile);
 }
-$this->smarty->display('main.tpl');
+$smarty->display('main.tpl');
