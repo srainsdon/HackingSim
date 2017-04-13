d70d3e0296229d6374a063ad41418eaf2288933c
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-12 21:22:34 -0700

    working on stuff
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/ipv4.class.php
+++ classes/ipv4.class.php
@@ -42,6 +42,7 @@ class ipv4
         // @list($ip, $len) = explode('/', $range);
 
     $startIP = $this->classCidr->cidr2network($this->address,$this->cidr);
+    echo $startIP;
         if (($min = ip2long($startIP)) !== false) {
             $max = ($min | (1 << (32 - $this->cidr)) - 1);
             for ($i = $min; $i < $max; $i++)
