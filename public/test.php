be408689fc86d5ae84ccb142a300185bd34cf7bb
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-12 20:04:39 -0700

    working on stuff
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/test.php
+++ public/test.php
@@ -19,7 +19,7 @@ echo "netmask2cidr: $netMask " . $cidr->netmask2cidr($netMask) . "<br />\n";
 echo "cidr_match: 28.237.245.45/24 " . $cidr->cidr_match('28.237.245.45', '28.237.245.0', '24') . "<br />\n";
 
 $netList = $sql->getNetworkList();
-echo "NetList:" . nl2br(print_r($netList)) . "<br />";
+echo "NetList:" . nl2br(print_r($netList , 1)) . "<br />";
 $all = $ip->getAllAddress();
 reset($all);
 echo "First: " . current($all) . " Last: " . end($all);
