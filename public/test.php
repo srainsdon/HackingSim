03c0e5b7dbda6886103efb9a00ccde485c81daeb
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-12 18:29:00 -0700

    working on stuff
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/test.php
+++ public/test.php
@@ -6,4 +6,10 @@
  * Time: 6:25 PM
  */
 
-echo "Test: php Test";
+include_once '../config.php';
+
+$settings = new appSettings();
+
+$settings->setData('version', '0.12');
+$settings->setData('port', '5312');
+echo $settings->getData();
