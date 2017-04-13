f8cd20b2b3bc13767ab9651f5916953022640168
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-13 07:54:26 -0700

    stuff
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/commands.class.php
+++ classes/commands.class.php
@@ -9,9 +9,11 @@
 class commands
 {
     private $sql;
+    private $calculator;
 
     function __construct(sqlManager &$sql)
     {
+        $this->calculator = new calculator();
         $this->sql = $sql;
     }
 
@@ -20,6 +22,18 @@ class commands
         $_SESSION['CommandHistory'] .= "PING: $destinationIP from $originatingIP\n";
         $dest = $this->sql->getComputerByIP($destinationIP);
         $orig = $this->sql->getComputerByIP($originatingIP);
+        foreach ($dest['ComputerServices'] as $service) {
+            if ($service['name'] == 'ping'){
+                if(isset($service['ports'][1]['inbound'])) {
+                    list($ip,$mask) = explode('/', $service['ports'][1]['inbound']);
+                    if($this->calculator->cidr_match($originatingIP,$ip,$mask)) {
+                        $this->pong();
+                    } else {
+                        $_SESSION['CommandHistory'] .= 'Humm...\n'
+                    }
+                }
+            }
+        }
         $_SESSION['CommandHistory'] .= print_r($dest,true);
     }
 
