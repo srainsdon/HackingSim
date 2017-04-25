5414474e5b6efc3f19956fa935fbe4e81f0cd1a9
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-25 16:39:54 -0700

    working out the new router testing
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/index.php
+++ public/index.php
@@ -1,20 +1,21 @@
 <?php
 require '../vendor/autoload.php';
 $router = new AltoRouter();
-$router->map('GET|POST','/', 'home', 'home');
-$router->map( 'GET', '/user/[:action]/', function( $action ) {
+$router->map('GET|POST', '/', 'home', 'home');
+$router->map('GET', '/user/[:action]/', function ($action) {
     echo "Action: $action<br />\n";
+    echo "<pre>" . var_dump($match, true) . "</pre>";
 });
 // match current request
 $match = $router->match();
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} elseif ($match) {
+    ?>
+    <h1>AltoRouter</h1>
 
-
-if ($match) {
-?>
-<h1>AltoRouter</h1>
-
-<h3>Current request: </h3>
-<pre>
+    <h3>Current request: </h3>
+    <pre>
 <?php var_dump($match); ?>
 </pre>
 
