e448d75fc85ebafb4d50811f0d39c79483caf722
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-25 14:33:39 -0700

    working out the new router testing
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/router.php
+++ public/router.php
@@ -1,4 +1,5 @@
 <?php
+require '../vendor/autoload.php';
 $router = new AltoRouter();
 $router->map('GET|POST','/', 'home#index', 'home');
 $router->map('GET','/users/', array('c' => 'UserController', 'a' => 'ListAction'));
