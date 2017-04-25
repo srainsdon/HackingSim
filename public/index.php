40b8ebbdb93ff77559d2c8c93798f267e7a32093
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-25 16:28:45 -0700

    working out the new router testing
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/index.php
+++ public/index.php
@@ -1,24 +1,21 @@
 <?php
 require '../vendor/autoload.php';
 $router = new AltoRouter();
-$router->map('GET|POST','/', 'home#index', 'home');
-$router->map('GET','/users/', array('c' => 'UserController', 'a' => 'ListAction'));
-$router->map('GET','/users/[i:id]', 'users#show', 'users_show');
-$router->map('GET','/api/[json|xml:type]/[:request]', 'api#stuff', 'api' );
-$router->map('POST','/users/[i:id]/[delete|update:action]', 'usersController#doAction', 'users_do');
+$router->map('GET|POST','/', 'home', 'home');
+$router->map( 'GET', '/[:action]/', function( $action ) {
+    echo "Action: $action<br />\n";
+});
 // match current request
 $match = $router->match();
+
+
+if ($match) {
 ?>
 <h1>AltoRouter</h1>
 
 <h3>Current request: </h3>
 <pre>
-	Target: <?php var_dump($match['target']); ?>
-    Params: <?php var_dump($match['params']); ?>
-    Name: 	<?php var_dump($match['name']); ?>
+	<?php var_dump($match); ?>
 </pre>
 
-<h3>Try these requests: </h3>
-<p><a href="<?php echo $router->generate('home'); ?>">GET <?php echo $router->generate('home'); ?></a></p>
-<p><a href="<?php echo $router->generate('users_show', array('id' => 5)); ?>">GET <?php echo $router->generate('users_show', array('id' => 5)); ?></a></p>
-<p><form action="<?php echo $router->generate('users_do', array('id' => 10, 'action' => 'update')); ?>" method="post"><button type="submit"><?php echo $router->generate('users_do', array('id' => 10, 'action' => 'update')); ?></button></form></p>
+<?php }
