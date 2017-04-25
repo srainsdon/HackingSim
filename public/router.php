b48e6756e4e24c421decf2d1c1a5e7e3888ad3f2
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-25 14:12:36 -0700

    working out the new router testing
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/router.php
+++ public/router.php
@@ -6,6 +6,8 @@
  * Time: 1:06 PM
  */
 
+phpinfo();
+/*
 include_once '../config.php';
 $log->debug("Router.php - File");
 $router = new AltoRouter();
@@ -14,4 +16,4 @@ $router->map( 'GET', '/', function() {
     echo "root";
 });
 
-$router->map( 'GET', '/[:action]/', "phpinfo" );
+$router->map( 'GET', '/[:action]/', "phpinfo" ); */
