ba75cbcad4641e10a55481544dff72b741db5923
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 06:48:19 -0700

    working out Namespaces Going with HackSim as our base
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- classes/HackSim/Database/core.php
+++ classes/HackSim/Database/core.php
@@ -23,11 +23,11 @@ class core extends Singleton
         //$this->log->debug("Core: Loading...");
         $this->dsn = "mysql:host=".getenv('dbHost').";dbname=".getenv('dbDatabase').";charset=utf8";
         $opt = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false,
         ];
         $this->pdo = new \PDO($this->dsn, getenv('dbUser'), getenv('dbPass'), $opt);
-        var_dump($this->pdo->getAttribute());
+        var_dump($this->pdo->exec("SELECT VERSION();"));
     }
 }
