e443415328867a899538b2e22ea0e131a36990f8
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 15:42:46 -0700

    working out Namespaces working out logging
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/logs.php
+++ public/logs.php
@@ -1,5 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 require '../vendor/autoload.php';
+
 Logger::configure($_SERVER["DOCUMENT_ROOT"] . '/../log4php.xml');
 $smarty = new Smarty_HackingSim();
 $smarty->append('bCrumbs', "<span class=\"breadcrumb-item active\">Log Reader</span>");
