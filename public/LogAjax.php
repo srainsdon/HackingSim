a0e072d85d10ce77dd0bbcea71299fc2493cc6bc
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 10:53:22 -0700

    working out Namespaces Going with HackSim as our base
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/LogAjax.php
+++ public/LogAjax.php
@@ -15,4 +15,4 @@ ini_set("display_errors", 1);
 require_once('../vendor/autoload.php');
 Logger::configure($_SERVER["DOCUMENT_ROOT"] . '/../log4php.xml');
 $dev = new \HackSim\Core\Development();
-echo $dev->getLogTail();
+echo implode("\n", $dev->getLogTail());
