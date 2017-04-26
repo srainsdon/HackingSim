b53599238406653d8ce7076a058da0df79c70357
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 08:51:40 -0700

    working out Namespaces Going with HackSim as our base
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/test.php
+++ public/test.php
@@ -3,3 +3,4 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 require_once ('../vendor/autoload.php');
 $corePDO = \HackSim\Database\core::getInstance();
+echo nl2br(print_r(\HackSim\Database\user::getUserData(), true));
