83c677f703b773758eef4bcaf91625f0aca412a2
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 13:17:43 -0700

    working out Namespaces Going with HackSim as our base
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/logs.php
+++ public/logs.php
@@ -1,6 +1,9 @@
 <html>
 <head>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 <body>
