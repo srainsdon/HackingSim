3a274c5e2db26870bec43f222508010142edf2f7
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 12:18:43 -0700

    working out Namespaces Going with HackSim as our base
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/logs.php
+++ public/logs.php
@@ -1,28 +1,38 @@
 <html>
 <head>
     <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript">
+
+        function loaddata() {
+            var name = document.getElementById("username");
+
+            if (name) {
+                $.ajax({
+                    type: 'post',
+                    url: 'LogAjax.php',
+                    data: {
+                        timestamp: name,
+                    },
+                    success: function (response) {
+                        // We get the element having id of display_info and put the response inside it
+                        $('#display_info').html(response);
+                    }
+                });
+            }
+
+            else {
+                $('#display_info').html("Please Enter Some Words");
+            }
+        }
+
+    </script>
 
 </head>
 <body>
-<button id="loadbasic">basic load</button>
+
+<input type="text" name="username" id="username" onkeyup="loaddata();">
 <div id="display_info">
 </div>
-<script>
-    $(function () {
-// don't cache ajax or content won't be fresh
-        $.ajaxSetup({
-            cache: false
-        });
-        var ajax_load = "<img src='http://i.imgur.com/pKopwXp.gif' alt='loading...'/>";
 
-// load() functions
-        var loadUrl = "/LogAjax.php";
-        $("#loadbasic").click(function () {
-            $("#display_info").html(ajax_load).load(loadUrl);
-        });
-
-// end
-    });
-</script>
 </body>
 </html>
