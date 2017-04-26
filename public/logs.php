77a160e378662458969f21f213d97dd18d525822
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 10:17:13 -0700

    working out Namespaces Going with HackSim as our base
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

---
+++ public/logs.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript">
+
+        function loaddata() {
+            var name = document.getElementById("username");
+
+            if (name) {
+                $.ajax({
+                    type: 'post',
+                    url: 'LogAjax.php',
+                    data: {
+                        user_name: name,
+                    },
+                    success: function (response) {
+                        // We get the element having id of display_info and put the response inside it
+                        $('#display_info').html(response);
+                    }
+                });
+            }
+
+            else {
+                $('#display_info').html("Please Enter Some Words");
+            }
+        }
+
+    </script>
+
+</head>
+<body>
+
+<input type="text" name="username" id="username" onkeyup="loaddata();">
+<div id="display_info">
+</div>
+
+</body>
+</html>
