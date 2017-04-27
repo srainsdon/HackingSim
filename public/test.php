0e6d319eae2b3ebbf5b51a0be48ada1588e60285
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 23:05:52 -0700

    working out Namespaces working out logging
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/test.php
+++ public/test.php
@@ -2,8 +2,5 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 require_once('../vendor/autoload.php');
-Logger::configure($_SERVER["DOCUMENT_ROOT"] . '/../log4php.xml');
-$userDB = new HackSim\Database\User();
-echo "<pre>" . print_r($userDB->getUserData('srainsdon@nunetnetworks.net'), true) . "</pre>";
-$dev = new \HackSim\Core\Development();
-echo "<pre>" . $dev->getLogTail() . "</pre>";
+$configurator = new LoggerConfiguratorDefault();
+$config = $configurator->parse('/../log4php.xml');
