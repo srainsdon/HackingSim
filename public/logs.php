6735e2424972ba385fdf71c476d2b5eb5d1293a9
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-04-26 19:46:19 -0700

    working out Namespaces working out logging
    Signed-off-by: Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>

--- public/logs.php
+++ public/logs.php
@@ -8,6 +8,6 @@ Logger::configure($_SERVER["DOCUMENT_ROOT"] . '/../log4php.xml');
 $smarty = new Smarty_HackingSim();
 $smarty->append('bCrumbs', "<span class=\"breadcrumb-item active\">Log Reader</span>");
 $dev = new \HackSim\Core\Development();
-$data = $dev->getLogTail($rows);
+$data = $dev->getLogTail(50);
 $smarty->assign('logs', $data);
 $smarty->display('logs.tpl');
